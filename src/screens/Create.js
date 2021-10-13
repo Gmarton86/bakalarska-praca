6bb9b1ea9843bdf7503158ba4bf9b01d163c3245
ErikZurvalec <xzurvalec@stuba.sk>
2021-10-13 22:03:37 +0200

    players added

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,33 +1,36 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import BackButton from "../utils/backButton";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import BackButton from '../utils/backButton'
 
 export default function Create({ navigation }) {
-  const visitHome = () => {
-    navigation.replace("Home");
-  };
+    const visitHome = () => {
+        navigation.replace('Home')
+    }
 
-  return (
-    <View style={styles.body}>
-      {/* <View style={styles.backButton}>
-        <BackButton title="back" onPressFunction={visitHome} />
-      </View> */}
-      <BackButton goBack={visitHome} />
-      <View>
-        <Text>TTT creation</Text>
-      </View>
-    </View>
-  );
+    return ( <
+        View style = { styles.body } > {
+            /* <View style={styles.backButton}>
+                    <BackButton title="back" onPressFunction={visitHome} />
+                  </View> */
+        } <
+        BackButton goBack = { visitHome }
+        /> <
+        View >
+        <
+        Text > TTT creation < /Text> <
+        /View> <
+        /View>
+    )
 }
 
 const styles = StyleSheet.create({
-  backButton: {
-    flex: 1,
-    backgroundColor: "#000000",
-    justifyContent: "center",
-    alignItems: "flex-start",
-  },
-  body: {
-    flex: 1,
-  },
-});
+    backButton: {
+        flex: 1,
+        backgroundColor: '#000000',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+    body: {
+        flex: 1,
+    },
+})
