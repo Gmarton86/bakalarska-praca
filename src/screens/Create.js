42ea079060a3bad7c501440941fa85711416a7a9
ErikZurvalec <xzurvalec@stuba.sk>
2021-10-14 11:42:57 +0200

    players added

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,36 +1,46 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import BackButton from '../utils/backButton'
 
 export default function Create({ navigation }) {
-    const visitHome = () => {
-        navigation.replace('Home')
-    }
+  const visitHome = () => {
+    navigation.replace('Home')
+  }
 
-    return ( <
-        View style = { styles.body } > {
-            /* <View style={styles.backButton}>
-                    <BackButton title="back" onPressFunction={visitHome} />
-                  </View> */
-        } <
-        BackButton goBack = { visitHome }
-        /> <
-        View >
-        <
-        Text > TTT creation < /Text> <
-        /View> <
-        /View>
-    )
+  return (
+    <View style={styles.body}>
+      <BackButton goBack={visitHome} />
+      <Text style={styles.text}>Tournament creation</Text>
+      <Text>Zadaj meno turnaja:</Text>
+      <TextInput
+        style={styles.input}
+        label="Name"
+        placeholder="Majstrovstva Oravy"
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    backButton: {
-        flex: 1,
-        backgroundColor: '#000000',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-    },
-    body: {
-        flex: 1,
-    },
+  backButton: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  body: {
+    flex:1,
+    backgroundColor: "#FFFFFF",
+  },
+  text: {
+    color: "#000000",
+    fontSize: 32,
+    fontStyle: "italic",
+    marginTop: 30,
+  },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    borderRadius: 5,
+    textAlign: "center",
+    fontSize: 20,
+  }
 })
