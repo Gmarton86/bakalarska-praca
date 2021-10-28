0f6cb6013fcc13dfddcecd0dc37f2352c37b75c7
ErikZurvalec <xzurvalec@stuba.sk>
2021-10-28 11:16:35 +0200

    players are sorted

--- src/screens/Tournament.js
+++ src/screens/Tournament.js
@@ -1,14 +1,57 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet, FlatList } from 'react-native'
 import BackButton from '../utils/backButton'
+import SQLite from "react-native-sqlite-storage"
+
+const db = SQLite.openDatabase(
+  {
+    name: 'LoginDB',
+    location: 'default',
+    createFromLocation: '../db/LoginDB.db',
+  },
+  () => {},
+  (error) => {
+    console.log(error)
+  }
+)
 
 export default function Tournament({ route, navigation }) {
   const [match, setMatch] = useState([{ value: 'item' }, {value: 'ok'}])
+  const [player, setPlayer] = useState([])
   const { name } = route.params;
 
+
   useEffect(() => {
-    console.log(name)
+    passTournamentData()
   }, [])
+
+  const passTournamentData = async () => {
+    try {
+      db.transaction((tx) => {
+        tx.executeSql(
+          'SELECT Players.Name, Players.Username, Players.Rank FROM Players JOIN Tournaments ON Tournaments.PlayerID = Players.ID WHERE Tournaments.Name = ?',
+          [name],
+          (tx, results) => {
+            var len = results.rows.length
+            for(let i = 0; i < len; i++){
+              let name = results.rows.item(i).Name + ' ' + results.rows.item(i).Username
+              let rank = results.rows.item(i).Rank
+              let c = {name, rank}
+              player.push(c)
+              console.log(player[i])
+            }
+            console.log('\n')
+            player.sort((a, b) => {return parseInt(a.rank) - parseInt(b.rank)})
+            for(let i = 0; i < player.length; i++){
+              console.log(player[i])
+            }
+          }
+        )
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const visitHome = () => {
     navigation.replace('Home')
@@ -19,7 +62,7 @@ export default function Tournament({ route, navigation }) {
       <View>
         <BackButton goBack={visitHome} />
       </View>
-      <Text style={styles.text}>Tournament</Text>
+      <Text style={styles.text}>Rozpis zapasov</Text>
       <FlatList
         keyExtractor={(item, index) => index.toString()}
         data={match}
