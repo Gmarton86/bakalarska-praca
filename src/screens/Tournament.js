bb689da80be0a7c15f5b27d0c17dbf9312bd3cbc
ErikZurvalec <xzurvalec@stuba.sk>
2021-11-10 11:34:53 +0100

    Tailwind added

--- src/screens/Tournament.js
+++ src/screens/Tournament.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, FlatList, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, FlatList, ScrollView, TextInput } from 'react-native'
 import BackButton from '../utils/backButton'
 import SQLite from 'react-native-sqlite-storage'
+import tw from 'tailwind-react-native-classnames'
 
 const db = SQLite.openDatabase(
   {
@@ -16,9 +17,14 @@ const db = SQLite.openDatabase(
 )
 
 export default function Tournament({ route, navigation }) {
-  const [match, setMatch] = useState([{ playerOne: 'a', playerTwo: 'b' }])
+  const [match, setMatch] = useState([
+    { playerOne: 'a', playerTwo: 'b', table: 0, winner: 1 },
+  ])
   const [player1, setPlayer1] = useState([])
   const [player2, setPlayer2] = useState([])
+  const [tables, setTables] = useState([])
+  const [winners, setWinners] = useState([])
+  const [matchWinner, setMatchWinner] = useState({value: 'none'})
   const { name } = route.params
 
   useEffect(() => {
@@ -34,23 +40,23 @@ export default function Tournament({ route, navigation }) {
         tx.executeSql(
           'SELECT Player1ID, Player2ID FROM Matches WHERE Matches.TournamentName=?',
           [name],
-           (err, results) => {
+          (err, results) => {
             var len = results.rows.length
-        
+
             for (let i = 0; i < len; i++) {
-               tx.executeSql(
+              tx.executeSql(
                 'SELECT Name, Username FROM Players WHERE Players.ID=?',
                 [results.rows.item(i).Player1ID],
-                 (tx, result1) => {
+                (tx, result1) => {
                   playerOne =
                     result1.rows.item(0).Name +
                     ' ' +
                     result1.rows.item(0).Username
 
-                    player1.push(playerOne)
+                  player1.push(playerOne)
                 }
               )
-              if(results.rows.item(i).Player2ID != 0){
+              if (results.rows.item(i).Player2ID != 0) {
                 tx.executeSql(
                   'SELECT Name, Username FROM Players WHERE Players.ID=?',
                   [results.rows.item(i).Player2ID],
@@ -66,6 +72,8 @@ export default function Tournament({ route, navigation }) {
                 playerTwo = ''
                 player2.push(playerTwo)
               }
+              tables.push(results.rows.item(i).Stol)
+              winners.push(results.rows.item(i).WinnerID)
             }
           }
         )
@@ -78,16 +86,22 @@ export default function Tournament({ route, navigation }) {
 
   const wait = () => {
     match.pop()
-    for(let i = 0; i < player1.length; i++){
+    for (let i = 0; i < player1.length; i++) {
       var playerOne = player1[i]
       var playerTwo = player2[i]
-      var c = { playerOne, playerTwo}
+      var table = tables[i]
+      var winner = winners[i]
+      var c = { playerOne, playerTwo, table, winner }
       console.log(c)
       match.push(c)
     }
-    for (let i = 0; i < match.length; i++) {
-      console.log(match[i])
-    }
+    // for (let i = 0; i < match.length; i++) {
+    //   console.log(match[i])
+    // }
+  }
+
+  const setWinner = () => {
+
   }
 
   const visitHome = () => {
@@ -104,9 +118,35 @@ export default function Tournament({ route, navigation }) {
         keyExtractor={(item, index) => index.toString()}
         data={match}
         renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Text style={styles.text}>{item.playerOne}</Text>
-            <Text style={styles.text}>{item.playerTwo}</Text>
+          <View
+            style={tw.style(
+              'flex',
+              'flex-col',
+              'bg-blue-400',
+              'm-1',
+              'rounded-md'
+            )}
+          >
+            <View style={tw.style('flex-1')}>
+              <Text style={styles.text}>Hráč 1: {item.playerOne}</Text>
+              <Text style={styles.text}>Hráč 2: {item.playerTwo}</Text>
+            </View>
+            <View style={tw.style('flex-1')}>
+              <Text style={styles.text}>Stôl: {item.table}</Text>
+            </View>
+            <View style={tw.style('flex-1')}>
+              <Text style={styles.text, tw.style('hidden')}>Víťaz: {item.winner}</Text>
+              <Text style={styles.text}>Zadaj víťaza:</Text>
+              <View style={tw.style('bg-yellow-500', 'h-10')}>
+              <TextInput
+                style={tw.style('w-full', 'text-3xl')}
+                label="Víťaz"
+                returnKeyType="next"
+                value={matchWinner.value}
+                onChangeText={setWinner}
+              />
+              </View>
+            </View>
           </View>
         )}
       />
